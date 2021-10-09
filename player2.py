5858c32080b0f2137ab419ef8f339e50ab4d8ea3
Jose Cuadra <jose.r.cuadra@gsk.com>
2021-10-09 15:21:17 -0400

    because playsound doesnt buffer for raspberry pi

---
+++ player2.py
@@ -0,0 +1,23 @@
+from pygame import mixer
+
+import readchar, os, random, copy
+
+data = os.listdir("audio/")
+files = copy.copy(data)
+
+mixer.init()
+
+while True:
+    letter = readchar.readkey()
+
+    if len(files) <= 1:
+        files = copy.copy(data)
+
+    choice = random.choice(files)
+    files.remove(choice)
+
+    print(choice.replace(".mp3", ""))
+
+    mixer.music.load("audio/"+choice)
+    mixer.music.set_volume(0.7)
+    mixer.music.play()
