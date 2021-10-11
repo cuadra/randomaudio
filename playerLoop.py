790d52c7bea2bc9abf22a1da79664fd64f920546
Jose Cuadra <jose.r.cuadra@gsk.com>
2021-10-11 07:55:09 -0400

    reducing volume even more

--- playerLoop.py
+++ playerLoop.py
@@ -1,7 +1,7 @@
 import pygame
 pygame.mixer.init()
 pygame.mixer.music.load("audio/ambience.mp3")
-pygame.mixer.music.set_volume(.5)
+pygame.mixer.music.set_volume(.2)
 
 while True:
   pygame.mixer.music.play()
